6f980c9606e6957e0b12b834e95f45c0b891684a
Leo Ferreira <leonardoferreira@protonmail.com>
2022-12-06 23:22:35 -0300

    feat: write protobuf to file and parse file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Course.Protobuf.Test;
+
+Console.WriteLine("protobuf test");
+
+var emp = new Employee();
+
+emp.FirstName = "foo";
+emp.LastName = "bar";
+emp.IsRetired = false;
+var birthDate = new DateTime(1999, 7, 9);
+birthDate = DateTime.SpecifyKind(birthDate, DateTimeKind.Utc);
+emp.BirthDate = Timestamp.FromDateTime(birthDate);
+emp.MaritalStatus = Employee.Types.MaritalStatus.Married;
+emp.PreviousEmployers.Add("company1");
+emp.PreviousEmployers.Add("company2");
+
+using (var output = File.Create("emp.dat"))
+{
+  emp.WriteTo(output);
+}
+
+Employee empFromFile;
+using (var input = File.OpenRead("emp.dat"))
+{
+  empFromFile = Employee.Parser.ParseFrom(input);
+}
+
+Console.WriteLine("protobuf test done");
